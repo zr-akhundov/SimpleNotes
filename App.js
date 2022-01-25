539f52b6789f64a8db7c845a6726dae7aaa79fa3
Zaur Axundov <zr.akhundov@gmail.com>
2022-01-26 01:04:28 +0400

    react-native init

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+class App extends React.Component {
+	render() {
+		return (
+			<View>
+				
+			</View>
+		);
+	}
+};
+
+export default App;
